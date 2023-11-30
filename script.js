6051a2b1873229374780211ff4911a09489bab3e
abbottpRConnects <152523870+abbottpRConnects@users.noreply.github.com>
2023-11-30 12:00:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.onload = function() {
-    // Find the play button
-    var playButton = document.querySelector('.fp-play .fp-icon.fp-playbtn');
+    // Delay function execution by 5000 milliseconds (5 seconds)
+    setTimeout(function() {
+        // Find the play button
+        var playButton = document.querySelector('.fp-play .fp-icon.fp-playbtn');
 
-    // Check if the play button exists
-    if (playButton) {
-        // Simulate a click on the play button
-        playButton.click();
-        console.log("Play button clicked");
-    } else {
-        console.log("Play button not found");
-    }
+        // Check if the play button exists
+        if (playButton) {
+            // Simulate a click on the play button
+            playButton.click();
+            console.log("Play button clicked");
+        } else {
+            console.log("Play button not found");
+        }
+    }, 5000); // 5000 milliseconds = 5 seconds
 };
