2e1613fb6fba1fbeb43f3fd9090af9d537d40440
abbottpRConnects <152523870+abbottpRConnects@users.noreply.github.com>
2023-11-30 10:56:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // Find the play button
-    var playButton = document.querySelector('.fp-play .fp-icon.fp-playbtn');
+    var playButton = document.querySelector('videoEmbed');
 
     // Check if the play button exists
     if (playButton) {
