5d71007277c5df0d8b51e1df0437263aa038d131
abbottpRConnects <152523870+abbottpRConnects@users.noreply.github.com>
2023-11-30 10:59:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 window.onload = function() {
-    // Find the play button
-    var playButton = document.querySelector('videoEmbed');
+    // Delay function execution by 5000 milliseconds (5 seconds)
+    setTimeout(function() {
+        // Find the play button
+        var playButton = document.getElementById('videoEmbed');
 
-    // Check if the play button exists
-    if (playButton) {
-        // Simulate a click on the play button
-        playButton.click();
-    } else {
-        console.log("Play button not found");
-    }
+        // Check if the play button exists
+        if (playButton) {
+            // Simulate a click on the play button
+            playButton.click();
+        } else {
+            console.log("Play button not found");
+        }
+    }, 5000); // 5000 milliseconds = 5 seconds
 };
