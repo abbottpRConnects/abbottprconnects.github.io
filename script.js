2550f2bf9ce1f96ef4db11eebf3b96f55505f5ed
abbottpRConnects <152523870+abbottpRConnects@users.noreply.github.com>
2023-11-30 10:54:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 window.onload = function() {
-    var embed = document.getElementById('videoEmbed');
+    // Find the play button
+    var playButton = document.querySelector('.fp-play .fp-icon.fp-playbtn');
 
-    if (embed) {
-        // Request Fullscreen
-        if (embed.requestFullscreen) {
-            embed.requestFullscreen();
-        } else if (embed.mozRequestFullScreen) { /* Firefox */
-            embed.mozRequestFullScreen();
-        } else if (embed.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-            embed.webkitRequestFullscreen();
-        } else if (embed.msRequestFullscreen) { /* IE/Edge */
-            embed.msRequestFullscreen();
-        }
+    // Check if the play button exists
+    if (playButton) {
+        // Simulate a click on the play button
+        playButton.click();
     } else {
-        console.log("Embed element not found");
+        console.log("Play button not found");
     }
 };
