e2cfe5a3a0f1a31960aebdfdb20c34515b33c591
abbottpRConnects <152523870+abbottpRConnects@users.noreply.github.com>
2023-11-30 12:41:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Wait for the DOM to be fully loaded
+/*// Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function() {
         // Find the element with the specified classes
@@ -13,4 +13,24 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log("Play button not found");
         }
     }, 5000); // Delay for 5000 milliseconds (5 seconds)
-});
+});*/
+
+// Function to execute when mutations are observed
+var observerCallback = function(mutationsList, observer) {
+    let playButton = document.querySelector('.fp-play.fp-visible .fp-icon.fp-playbtn');
+    if (playButton) {
+        playButton.click();
+        observer.disconnect(); // Stop observing after the click
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(observerCallback);
+
+// Start observing the document body for DOM mutations
+observer.observe(document.body, { childList: true, subtree: true });
+
+// Set a timeout to stop observing after a certain period if the button is not found
+setTimeout(() => {
+    observer.disconnect();
+}, 10000); // Adjust time as necessary
