38c1a693c84946d7442e9c6df153494ff31c7285
abbottpRConnects <152523870+abbottpRConnects@users.noreply.github.com>
2023-11-30 11:20:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-window.onload = function() {
-    // Delay function execution by 5000 milliseconds (5 seconds)
-    setTimeout(function() {
-        // Find the play button
-        var playButton = document.querySelector('.fp-icon.fp-playbtn');
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Find the element with the specified classes
+    var playButton = document.querySelector('.fp-icon.fp-playbtn');
 
-        // Check if the play button exists
-        if (playButton) {
-            // Simulate a click on the play button
-            playButton.click();
-        } else {
-            console.log("Play button not found");
-        }
-    }, 1000); // 1000 milliseconds = 1 second
-};
+    // Check if the element exists
+    if (playButton) {
+        // Programmatically click the element
+        playButton.click();
+    } else {
+        console.log("Play button not found");
+    }
+});
